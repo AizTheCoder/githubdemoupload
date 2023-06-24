8384fbe6109875fffcbcc8fe8cd250ebdce422f4
Muhammed AIZ <muhammedaiz16@gmail.com>
2023-06-24 13:52:08 +0400

    Initial Commit

---
+++ GithubDemoUpload/GithubDemoUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoUploadApp.swift
+//  GithubDemoUpload
+//
+//  Created by Muhammed Aiz on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
